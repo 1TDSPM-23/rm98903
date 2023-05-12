6ceb4bff7963629043d2eb498772fa9089f260cd
VitorKubica <vitor05kubica12@gmail.com>
2023-05-12 09:46:39 -0300

    atualização das paginas

---
+++ js/calc.js
@@ -0,0 +1,29 @@
+
+function insereNumeroNoVisor(botao,visor) {
+
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+
+        //Verificando o penúltimo caractere do visor utilizando o atributo length
+        if(visor.value[visor.value.length-1] == "+" || visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+            visor.value.slice(1);
+        }else{
+            visor.value += botao.value;
+        }
+
+    }else{
+        visor.value += botao.value;
+    }
+
+
+    
+}
+
+const resultadoDasOperacoes = (visor) =>{
+    //Utilizando a função eva(expressão)
+    visor.value = eval(visor.value);
+}
+
+function apagarOperacao(botao,visor) {
+    visor.value = null;
+    
+}
